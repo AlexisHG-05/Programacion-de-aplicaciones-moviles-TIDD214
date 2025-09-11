59ee9a7d29eddc5e692cb49a6b529ccc28fb4302
AlexisHG-05 <Ah9267992@gmail.com>
2025-09-11 10:14:50 -0600

    Eliminamos ejemplos

--- JSParaPrincipiantes/JSParaPrincipiantes/APP.js
+++ JSParaPrincipiantes/JSParaPrincipiantes/APP.js
@@ -1,16 +1,2 @@
-// estoy creando variables
-var nameuser = "john";
-let lastname = "carter";
-
-// reasignado el valor de la variable nameuser
-nameuser = 'pepe';
-
-// cualquiercosas
-const PI = 3.1415;
-
-console.log(PI);
-
-// nomasdasdasdasdasdasdasdasdddddddddddddddddasdasdasdadasdasd
-let userName = 'gordon';
 
 
