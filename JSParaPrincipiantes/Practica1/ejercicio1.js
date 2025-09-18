2ec3fb71ee1559fc3050c8c33f62549ef6dc4307
AlexisHG-05 <Ah9267992@gmail.com>
2025-09-18 00:33:03 -0600

    Resuelve el ejercicio a
    Corrección de variables

---
+++ JSParaPrincipiantes/Practica1/ejercicio1.js
@@ -0,0 +1,9 @@
+// a. Corrige el siguiente código
+let nombre = "Armando"; // Usamos let porque el nombre se reasigna más adelante
+const edad = 25; // Usamos const porque la edad no cambia
+
+nombre = "Ana Maria"; // Se reasigna el valor de nombre [cite: 16]
+
+// Se crea el saludo usando los valores actuales de las variables
+const saludo = `Hola, ${nombre}. Tienes ${edad} años.`; // Usamos ` (template literals) para un código más limpio
+
