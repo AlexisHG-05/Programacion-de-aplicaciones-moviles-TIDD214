80f707d26f69b0b7a6a18af004ab4a036a94928e
AlexisHG-05 <Ah9267992@gmail.com>
2025-11-07 15:31:00 -0600

    Segundo commit

--- JSParaPrincipiantes/Examen2P/Examen2doP/App.js
+++ JSParaPrincipiantes/Examen2P/Examen2doP/App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect}  from 'react';
-import { StyleSheet, Text, View, ImageBackground, Button,  } from 'react-native';
+import {View, Text, StyleSheet, ImageBackground, Button, SafeAreaView } from 'react-native';
 // splashcreen, iMAGEBACKGROUND,
 
 export default function Examen2P (){
@@ -9,19 +9,43 @@ export default function Examen2P (){
     const timer = setTimeout(() => {
       setShowplash(false);
     }, 4000);
-
-  
-
+    return () => clearTimeout(timer);
 }, []);
  if (showSplash) {
-    return (
+   return (
       <View style={styles.splashContainer}>
-        <Text style={styles.splashText}>Bienvenido</Text>
+        <Text style={styles.splashText}>Bienvenido a mi perfil</Text>
       </View>
     );
   }
-   return (
-    <SafeAreaView style={{ flex: 1 }}></SafeAreaView>
+  
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      
+      {}
+      <ImageBackground
+        source={{
+          uri: 'https://tse3.mm.bing.net/th/id/OIP.UM9VCCoPqFyf6mrKp2-RfQHaHa?rs=1&pid=ImgDetMain&o=7&rm=300',
+        }} 
+        style={styles.background} 
+      >
+        {}
+        <View style={styles.overlay}>
+          {}
+          <Text style={styles.title}>Alexis Hernandez Gutierrez</Text>
+          <Text style={styles.title}>Ingeniero en Tecnologias de la informacion e innovacion digital</Text>
+          <Text style={styles.title}>Me gusta demasiado escuchar musica en todo momento, cuando programo o investigo nuevos temas relacionados a mi carrera.</Text>
+          <Text style={styles.title}>Correo:Ah9267992@gmail.com.</Text>
+          <Text style={styles.title}>Numero:7122754948</Text>
+          
+
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
+
+  
+  );
 }
 
 const styles = StyleSheet.create({
@@ -30,5 +54,47 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },splashContainer: {
+    flex: 1, 
+    backgroundColor: '#000000ff', 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+  },
+
+  
+  splashText: {
+    fontSize: 24, 
+    color: '#fff', 
+  },
+
+  
+  background: {
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+  },
+
+  
+  overlay: {
+    backgroundColor: 'rgba(0,0,0,0.5)', 
+    padding: 20, 
+    borderRadius: 10, 
+  },
+
+  
+  title: {
+    fontSize: 28, 
+    color: '#fff', 
+    marginBottom: 10, 
+    textAlign: 'center', 
+  },
+    input: {
+    height: 50,
+    borderColor: 'gray',
+    borderWidth: 1, //Sirve para ver el borde del input
+    borderRadius: 8, // Bordes redondeados
+    paddingHorizontal: 15, // Espacio interno a los lados
+    marginBottom: 20, // Espacio debajo del input
+    fontSize: 16,
   },
 });
