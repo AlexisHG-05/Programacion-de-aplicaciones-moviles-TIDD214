cc7f677d9e6592b83f41f2bc64542fef0adfccd1
AlexisHG-05 <Ah9267992@gmail.com>
2025-11-19 23:49:19 -0600

    Practica No.17: Bottom Tabs Navigator
    Realice la practica de manera correcta

--- JSParaPrincipiantes/StackNavigation/App.js
+++ JSParaPrincipiantes/StackNavigation/App.js
@@ -1,19 +1,14 @@
-// ¡IMPORTANTE! Esta importación DEBE ser la primera línea de tu app.
 import 'react-native-gesture-handler';
 
-// a. Importamos las librerías instaladas
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-// b. Importamos los Screens creados
 import Home from './screens/home';
 import Profile from './screens/profile';
-import Settings from './screens/settings';
+import Settings from './screens/settings'
 
-// c. Creamos un Objeto Stack
 const Stack = createNativeStackNavigator();
 
-// d. Creamos el contenedor de navegación
 export default function App() {
   return (
     <NavigationContainer>
