72abd1b5671aacdf6c21c4c2d1c6851b1b56e7c6
AlexisHG-05 <Ah9267992@gmail.com>
2025-11-26 23:02:52 -0600

    Practica No.19: Insert y Select con SQLite Local
    Realice la practica de manera correcta.

---
+++ JSParaPrincipiantes/BaseDeDatos/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// Importamos la pantalla que creaste en la carpeta screens
+import InsertUsuarioScreen from './screens/InsertUsuarioScreen';
+
+export default function App() {
+  // Renderizamos directamente tu pantalla de práctica
+  return (
+    <InsertUsuarioScreen />
+  );
+}
